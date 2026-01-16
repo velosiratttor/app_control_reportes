d2346813d505dfb61edf29784e280f06902fef8f
Martin Pereira <pereiramartin1996@gmail.com>
2026-01-15 18:25:20 -0600

    primer commit de la app para respaldar primeros avances

---
+++ app_reporte.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+st.title("registro de actividades diarias")
+st.write("app de registro de actividades")
+
+nombre= st.text_input("Nombre del analista")
+fecha=st.date_input("fecha de la actividad")
+actividad=st.selectbox(
+    "Seleccione su actividad",
+    ["AUTORIZACIÓN PARA EL INGRESO DE CALIFICACIONES (AÑOS ANTERIORES)",
+     "REENVIO DE CERTIFICADOS DE CURSOS VIRTUALES",
+     "CORRECIÓN DE DATOS DE ESTUDIANTE/PROTAGONISTA",
+     "ASISTENCIA VIA TELEFONICA A C.T.",
+     "SOLICITUD PARA CAMBIO DE CORREO"],
+     index=None,
+     placeholder="Debes seleccionar tus acticvidades realizadas",
+     accept_new_options=True,
+)
+estado=st.selectbox(
+    "Estado de la actividad",
+    ["ATENDIDO",
+     "EN ATENCION",
+     "ESCLADO"],
+     index=None,
+     placeholder="seleccione el estado de tu actividad",
+     accept_new_options=True,
+)
+
+st.write("Nombre",nombre)
+st.write("Fecha",fecha)
+st.write("Actividad",actividad)
+st.write("Estado",estado)
